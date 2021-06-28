50e7f5bbaad00d4bb26f5a395b110d0da4b7bfc5
Joseph Salen <jsalen.91@gmail.com>
2021-06-28 19:05:14 -0400

    refactored swal alert to make it reusable

--- src/libs/helpers.js
+++ src/libs/helpers.js
@@ -1,3 +1,5 @@
+import swal from "sweetalert";
+
 export const formatCurrency = (money) => {
   const formatedCurrency = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -10,4 +12,17 @@ export const getTotalAmmount = (ammount) => {
   let totalAmmount = ammount.reduce((sum, { price }) => sum + price, 0);
 
   return totalAmmount;
+};
+
+export const confirmDeletion = async () => {
+  const confirmDeletion = await swal({
+    text: "¿Estás seguro que deseas borrar este elemento?",
+    buttons: {
+      cancel: "Cerrar",
+      confirm: "Eliminar",
+    },
+    icon: "warning",
+  });
+
+  return confirmDeletion;
 };
