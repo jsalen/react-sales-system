e3ed175b337a6f2ada693c1977b56120e757082e
Joseph Salen <jsalen.91@gmail.com>
2021-07-02 21:07:34 -0400

    added create, update and getProduct function

--- src/services/products.js
+++ src/services/products.js
@@ -10,10 +10,35 @@ export const getProducts = async () => {
   }
 };
 
+export const getProduct = async (id) => {
+  try {
+    const res = axios.get(URI + id);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
 export const deleteProduct = async (id) => {
   try {
     await axios.delete(URI + id);
   } catch (error) {
     console.log("error", error);
+  }
+};
+
+export const updateProduct = async (id, product) => {
+  try {
+    await axios.put(URI + id, product);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createProduct = async (product) => {
+  try {
+    await axios.post(URI, product);
+  } catch (error) {
+    console.log(error);
   }
 };
