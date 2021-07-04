002b067968d40ace0c7daac2b738741e55029f02
Joseph Salen <jsalen.91@gmail.com>
2021-07-03 23:29:46 -0400

    feature: if there is no data on productsList, then it'll show a message saying it instead of showing an error

--- src/components/Inventory/InventoryList.jsx
+++ src/components/Inventory/InventoryList.jsx
@@ -34,29 +34,35 @@ export default function InventoryList() {
 
   return (
     <tbody>
-      {products.map((product) => (
-        <tr key={product._id}>
-          <td>{product.product}</td>
-          <td>{product.quantity}</td>
-          <td>{formatCurrency(product.price)}</td>
-          <td className="d-flex justify-content-around">
-            <Button
-              variant="primary"
-              size="sm"
-              href={"/editProduct/" + product._id}
-            >
-              Editar
-            </Button>
-            <Button
-              variant="danger"
-              size="sm"
-              onClick={() => handleDelete(product._id)}
-            >
-              Eliminar
-            </Button>
-          </td>
+      {products === undefined || products.length === 0 ? (
+        <tr>
+          <td colSpan="4">No hay Datos</td>
         </tr>
-      ))}
+      ) : (
+        products.map((product) => (
+          <tr key={product._id}>
+            <td>{product.product}</td>
+            <td>{product.quantity}</td>
+            <td>{formatCurrency(product.price)}</td>
+            <td className="d-flex justify-content-around">
+              <Button
+                variant="primary"
+                size="sm"
+                href={"/editProduct/" + product._id}
+              >
+                Editar
+              </Button>
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={() => handleDelete(product._id)}
+              >
+                Eliminar
+              </Button>
+            </td>
+          </tr>
+        ))
+      )}
     </tbody>
   );
 }
