e17f52ef3b6a7d13b3295fbe836b98003dfd4601
Joseph Salen <jsalen.91@gmail.com>
2021-07-08 22:31:17 -0400

    created isAdmin state, so that the Inventory table shows Administrative options acording to the user's privileges

--- src/components/Inventory/InventoryList.jsx
+++ src/components/Inventory/InventoryList.jsx
@@ -4,7 +4,7 @@ import { formatCurrency, confirmDeletion } from "../../libs/helpers";
 
 import { getProducts, deleteProduct } from "../../services/products";
 
-export default function InventoryList() {
+export default function InventoryList({ isAdmin }) {
   const [products, setProducts] = useState([]);
 
   let isRendered = useRef(true);
@@ -44,22 +44,24 @@ export default function InventoryList() {
             <td>{product.product}</td>
             <td>{product.quantity}</td>
             <td>{formatCurrency(product.price)}</td>
-            <td className="d-flex justify-content-around">
-              <Button
-                variant="primary"
-                size="sm"
-                href={"/editProduct/" + product._id}
-              >
-                Editar
-              </Button>
-              <Button
-                variant="danger"
-                size="sm"
-                onClick={() => handleDelete(product._id)}
-              >
-                Eliminar
-              </Button>
-            </td>
+            {isAdmin && (
+              <td className="d-flex justify-content-around">
+                <Button
+                  variant="primary"
+                  size="sm"
+                  href={"/editProduct/" + product._id}
+                >
+                  Editar
+                </Button>
+                <Button
+                  variant="danger"
+                  size="sm"
+                  onClick={() => handleDelete(product._id)}
+                >
+                  Eliminar
+                </Button>
+              </td>
+            )}
           </tr>
         ))
       )}
--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Col } from "react-bootstrap";
 import InventoryList from "../components/Inventory/InventoryList";
+import SearchBar from "../components/SearchBar/SearchBar";
+
 import { Container, Button, Table } from "react-bootstrap";
 
 export default function Inventory() {
+  const [isAdmin, setIsAdmin] = useState(true);
+
   return (
     <Container className="mt-4 text-center inventory-container">
-      <h1>Administración de inventario</h1>
+      <h1>{isAdmin ? "Administración" : "Consulta"} de inventario</h1>
       <Button variant="primary" className="mb-4">
         <Link to="/addProduct" className="button">
           Nuevo Producto
         </Link>
       </Button>
-      <Table striped bordered hover className="inventory-table">
+      <Col className="col-md-6 offset-md-3">
+        <SearchBar />
+      </Col>
+      <Table striped bordered hover className="inventory-table mt-3">
         <thead className="thead-dark">
           <tr>
             <th>Producto</th>
             <th>Cantidad</th>
             <th>Precio Unitario</th>
-            <th>Acciones</th>
+            {isAdmin && <th>Acciones</th>}
           </tr>
         </thead>
-        <InventoryList />
+        <InventoryList isAdmin={isAdmin} />
       </Table>
     </Container>
   );
