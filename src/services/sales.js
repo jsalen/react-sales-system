9dab1ee0f9598dc0cac341ae7a34e6e8dcd470cd
Joseph Salen <jsalen.91@gmail.com>
2021-07-09 20:42:55 -0400

    now services/sales can update a product's quantity in DB after the sale has been made

--- src/services/sales.js
+++ src/services/sales.js
@@ -1,16 +1,37 @@
 import axios from "axios";
-const URI = "http://localhost:4000/api/sales/";
+const URI_SALES = "http://localhost:4000/api/sales/";
+const URI_PRODUCTS = "http://localhost:4000/api/products/";
 
 export const createSale = async (sale, total) => {
-  console.log(sale);
   const newSale = {
     products: sale,
     totalPrice: total,
   };
 
   try {
-    await axios.post(URI, newSale);
+    await axios.post(URI_SALES, newSale);
   } catch (error) {
     console.log(error);
   }
+  updateInventoryFromSale(sale);
 };
+
+async function updateInventoryFromSale(products) {
+  for (let product in products) {
+    const newQuantity =
+      products[product].quantity - products[product].qtyInCart;
+    const updatedProduct = {
+      ...products[product],
+      quantity: newQuantity,
+    };
+    const id = products[product]._id;
+
+    delete updatedProduct.qtyInCart;
+
+    try {
+      await axios.put(URI_PRODUCTS + id, updatedProduct);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
