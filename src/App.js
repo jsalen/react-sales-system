3e4122d07e25b24f01e1c4d607a721cd15d54de6
Joseph Salen <jsalen.91@gmail.com>
2021-08-18 17:37:22 -0400

    refactored App to make private routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./pages/styles/globals.css";
 
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import Navigation from "./components/Navigation/Navigation";
 import Landing from "./pages/Landing";
@@ -11,18 +12,72 @@ import Admin from "./pages/Admin";
 import Inventory from "./pages/Inventory";
 import AddProduct from "./pages/AddProduct";
 import Reports from "./pages/Reports";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
+  const { user: currentUser } = useSelector((state) => state.auth);
+
   return (
     <Router>
       <Navigation />
-      <Route exact path="/" component={Landing} />
-      <Route path="/shopping-car" component={SalesPanel} />
-      <Route path="/admin" component={Admin} />
-      <Route path="/inventory" component={Inventory} />
-      <Route path="/editProduct/:id" component={AddProduct} />
-      <Route path="/addProduct" component={AddProduct} />
-      <Route path="/reports" component={Reports} />
+      <Route
+        exact
+        path="/"
+        render={() => (currentUser ? <Landing /> : <Redirect to="/login" />)}
+      />
+      <Route
+        path="/shopping-car"
+        render={() => (currentUser ? <SalesPanel /> : <Redirect to="/login" />)}
+      />
+      <Route
+        path="/admin"
+        render={() => (currentUser ? <Admin /> : <Redirect to="/login" />)}
+      />
+      <Route
+        path="/inventory"
+        render={() => (currentUser ? <Inventory /> : <Redirect to="/login" />)}
+      />
+      <Route
+        path="/editProduct/:id"
+        render={() =>
+          currentUser.roles.includes("ROLE_ADMIN") ? (
+            <AddProduct />
+          ) : (
+            <Redirect to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/addProduct"
+        render={() =>
+          currentUser.roles.includes("ROLE_ADMIN") ? (
+            <AddProduct />
+          ) : (
+            <Redirect to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/reports"
+        render={() => (currentUser ? <Reports /> : <Redirect to="/login" />)}
+      />
+      <Route
+        path="/login"
+        render={() => (!currentUser ? <Login /> : <Redirect to="/" />)}
+      />
+      <Route
+        path="/register"
+        render={() =>
+          currentUser && currentUser.roles.includes("ROLE_ADMIN") ? (
+            <Register />
+          ) : (
+            <Redirect to="/" />
+          )
+        }
+      />
+      <Route path="/profile" component={Profile} />
     </Router>
   );
 }
